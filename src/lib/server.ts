755d49c0277c29f92a2773625f5b3309427d9e36
Shourya Pal <palshourya897@gmail.com>
2024-09-04 13:17:50 +0530

    fix: fixing server.ts

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -11,7 +11,7 @@ type Product = {
 };
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL + "/api",
   withCredentials: true,
 });
 
